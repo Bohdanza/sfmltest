65ac9533fe38988245fb3249a18ffec9f7ad0af4
Bohdanza <bohdanza87@gmail.com>
2025-02-09 17:41:56 +0200

    Cmake setup almost finished

---
+++ myclass.cpp
@@ -0,0 +1,8 @@
+#include "myclass.hpp"
+#include <iostream>
+
+Class1::Class1()
+{
+    x=-10;
+    std::cout<<"x="<<x<<"\n";
+}
---
+++ myclass.hpp
@@ -0,0 +1,7 @@
+class Class1
+{
+    public:
+        Class1();
+        int x;
+        void change_x();
+};
